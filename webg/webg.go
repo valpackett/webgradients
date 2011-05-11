483eaa93cabcd1864ad7118120010d5b5600a262
myfreeweb <me@myfreeweb.ru>
2011-05-11 17:01:50 +0400

    Apache License

--- webg/webg.go
+++ webg/webg.go
@@ -1,4 +1,18 @@
 package webg
+//  Web Gradients
+//  Copyright 2011 Grigory V. <me@myfreeweb.ru>
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
 
 import (
 //  "fmt"
@@ -71,6 +85,9 @@ func init() {
 func handler(w http.ResponseWriter, r *http.Request) {
   width := getnum(r, "width", 1)
   height := getnum(r, "height", 100)
+  if width > 4096 || height > 4096 {
+    w.
+  }
   start := getcolor(r, "start", "eeeeec")
   end := getcolor(r, "end", "d3d7cf")
   direction := getstr(r, "direction", "down")
