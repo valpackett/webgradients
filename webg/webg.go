648e673c0bb235c3c6d1ec552d0393acaa5ff7ce
myfreeweb <me@myfreeweb.ru>
2011-05-30 23:33:44 +0400

    gofmt'd

--- webg/webg.go
+++ webg/webg.go
@@ -15,74 +15,80 @@ package webg
 //   limitations under the License.
 
 import (
-  "io"
-  "fmt"
-  "http"
-  "bytes"
-  "image"
-  "image/png"
-  "encoding/hex"
-  "strings"
-  "strconv"
- 	"appengine"
+	"io"
+	"fmt"
+	"http"
+	"bytes"
+	"image"
+	"image/png"
+	"encoding/hex"
+	"strings"
+	"strconv"
+	"appengine"
 	"appengine/memcache"
 )
 
 // Getting values from HTTP GET. DRY, isn't it?
-func getstr(r *http.Request, s string, def string) string {
-  val := r.FormValue(s)
-  if val != "" {
-    res, _ := http.URLUnescape(val)
-    return res
-  }
-  return def
+func getstr(r *http.Request, s, def string) string {
+	val := r.FormValue(s)
+	if val != "" {
+		res, _ := http.URLUnescape(val)
+		return res
+	}
+	return def
 }
 
 func getnum(r *http.Request, s string, def int) int {
-  res, _ := strconv.Atoi(getstr(r, s, strconv.Itoa(def)))
-  return res
+	res, _ := strconv.Atoi(getstr(r, s, strconv.Itoa(def)))
+	return res
 }
 
-func getcolor(r *http.Request, s string, def string) string {
-  return strings.Replace(getstr(r, s, def), "#", "", -1)
+func getcolor(r *http.Request, s, def string) string {
+	return strings.Replace(getstr(r, s, def), "#", "", -1)
 }
 
 // The core. Hardcore.
 func hex_to_rgb(s string) image.NRGBAColor {
-  b, _ := hex.DecodeString(s)
-  return image.NRGBAColor{b[0], b[1], b[2], 0xff}
+	b, _ := hex.DecodeString(s)
+	return image.NRGBAColor{b[0], b[1], b[2], 0xff}
 }
-func gradient(i *image.NRGBA, s string, e string, dir string) {
-  start := hex_to_rgb(s)
-  end := hex_to_rgb(e)
-  if dir == "left" || dir == "up" {
-    start = end
-    end = hex_to_rgb(s)
-  }
-  height := i.Rect.Max.Y
-  width := i.Rect.Max.X
-  wh := height
-  if dir == "left" || dir == "right" {
-    wh = width
-  }
-  for y := 0; y < height; y++ {
-    for x := 0; x < width; x++ {
-      d := y
-      if dir == "left" || dir == "right" {
-        d = x
-      }
-      i.Set(x, y, image.NRGBAColor{
-        uint8(int(start.R) + int(float32(d) / float32(wh) * float32(int(end.R) - int(start.R)))),
-        uint8(int(start.G) + int(float32(d) / float32(wh) * float32(int(end.G) - int(start.G)))),
-        uint8(int(start.B) + int(float32(d) / float32(wh) * float32(int(end.B) - int(start.B)))),
-        255})
-    }
-  }
+func gradient(i *image.NRGBA, s, e, dir string) {
+	var start, end string
+	if dir == "left" || dir == "up" {
+		start = hex_to_rgb(e)
+		end = hex_to_rgb(s)
+	} else {
+		start = hex_to_rgb(s)
+		end = hex_to_rgb(e)
+	}
+	height := i.Rect.Max.Y
+	width := i.Rect.Max.X
+	var wh int
+	if dir == "left" || dir == "right" {
+		wh = width
+	} else {
+		wh = height
+	}
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			var d int
+			if dir == "left" || dir == "right" {
+				d = x
+			} else {
+				d = y
+			}
+			i.Set(x, y, image.NRGBAColor{
+				uint8(int(start.R) + int(float32(d)/float32(wh)*float32(int(end.R)-int(start.R)))),
+				uint8(int(start.G) + int(float32(d)/float32(wh)*float32(int(end.G)-int(start.G)))),
+				uint8(int(start.B) + int(float32(d)/float32(wh)*float32(int(end.B)-int(start.B)))),
+				255})
+		}
+	}
 }
 
 // Whoah!
 func init() {
-  http.HandleFunc("/make", handler)
+	http.HandleFunc("/make", handler)
 }
 
 func error(w http.ResponseWriter, t string) {
@@ -92,31 +98,31 @@ func error(w http.ResponseWriter, t string) {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-  width := getnum(r, "width", 1)
-  height := getnum(r, "height", 100)
-  if width > 4096 || height > 4096 {
-    error(w, "Too big!")
-    return
-  }
-  start := getcolor(r, "start", "eeeeec")
-  end := getcolor(r, "end", "d3d7cf")
-  direction := getstr(r, "direction", "down")
-  cachekey := fmt.Sprintf("%sx%s_%s_%s_%s", strconv.Itoa(width), strconv.Itoa(height), start, end, direction)
-  c := appengine.NewContext(r)
-  w.Header().Set("Content-Type", "image/png")
-  if pic, err := memcache.Get(c, cachekey); err == memcache.ErrCacheMiss {
-    buf := new(bytes.Buffer)
-    image := image.NewNRGBA(width, height)
-    gradient(image, start, end, direction)
-    png.Encode(buf, image)
-    memcache.Add(c, &memcache.Item{
-      Key: cachekey,
-      Value: buf.Bytes(),
-    })
-    io.Copy(w, buf)
-  } else if err == nil {
-    w.Write(pic.Value)
-  } else {
-    error(w, "Some weird error")
-  }
+	width := getnum(r, "width", 1)
+	height := getnum(r, "height", 100)
+	if width > 4096 || height > 4096 {
+		error(w, "Too big!")
+		return
+	}
+	start := getcolor(r, "start", "eeeeec")
+	end := getcolor(r, "end", "d3d7cf")
+	direction := getstr(r, "direction", "down")
+	cachekey := fmt.Sprintf("%sx%s_%s_%s_%s", strconv.Itoa(width), strconv.Itoa(height), start, end, direction)
+	c := appengine.NewContext(r)
+	w.Header().Set("Content-Type", "image/png")
+	if pic, err := memcache.Get(c, cachekey); err == memcache.ErrCacheMiss {
+		buf := new(bytes.Buffer)
+		image := image.NewNRGBA(width, height)
+		gradient(image, start, end, direction)
+		png.Encode(buf, image)
+		memcache.Add(c, &memcache.Item{
+			Key:   cachekey,
+			Value: buf.Bytes(),
+		})
+		io.Copy(w, buf)
+	} else if err == nil {
+		w.Write(pic.Value)
+	} else {
+		error(w, "Some weird error")
+	}
 }
