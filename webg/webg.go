1a6c2dc8d1ce36d7bbab270b96e0719b9827956c
myfreeweb <floatboth@me.com>
2012-04-20 22:34:11 +0400

    update copyright

--- webg/webg.go
+++ webg/webg.go
@@ -1,6 +1,5 @@
-package webg
 //  Web Gradients
-//  Copyright 2011 Grigory V. <floatboth@me.com>
+//  Copyright 2011-2012 Greg V <floatboth@me.com>
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
@@ -13,6 +12,7 @@ package webg
 //   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
+package webg
 
 import (
 	"io"
